3e2455d1b1118bd9937271973a31e20db12fc3fb
Vinny Murphy <vmurphy@cisco.com>
2017-01-28 16:58:11 -0500

    checkpoint

--- spl/models.py
+++ spl/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 class StrokeLength(models.Model):
     """Stroke Length is a measurement of how much traction we are getting on each
@@ -11,24 +13,9 @@ class StrokeLength(models.Model):
 
     # TODO: add user
     # SL = (LP – DG) / SC
-    pool_length = models.IntegerField(default=25)
-    glide_distance = models.IntegerField(
-        help=('An average fitness swimmer Glide Distance may be around '
-              '4 meters. A developed competitive swimmer may be '
-              'around 5 meters or more.'),
-        default=4)
-    stroke_count = models.IntegerField(
-        help=('After you push off and take that first underwater stroke '
-              '(count that as stroke #0) count how many strokes it takes you '
-              'to reach the other wall, without any extra glide when you are '
-              'about to reach it. This will be your Stroke Count, or SC '
-              '(aka SPL).'),
-        default=18)
-    wingspan = models.IntegerField(
-        help=('The first objectively measurable feature of truly good '
-              'freestyle technique is Stroke Length. By a great deal of data '
-              'crunching and observation we estimate that a good freestyle '
-              'stroke should be within 55% to 70% of a swimmer’s wingspan '
-              '(or height, if you only know that measurement – it is '
-              'approximately the same, give +/- 5%, and sufficient for '
-              'most swimmers to calculate by).'))
+    user = models.ForeignKey(User, related_name='User', null=True)
+    today = models.DateField(auto_now=True)
+    pool_length = models.PositiveSmallIntegerField(default=25)
+    glide_distance = models.PositiveSmallIntegerField(default=5)
+    stroke_count = models.PositiveSmallIntegerField(default=15)
+    wingspan = models.PositiveSmallIntegerField(default=68)
